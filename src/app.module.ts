2980cb0e1a2b60d295d54178ee0d8e8940085b7e
chemokita13 <chemokita13@gmail.com>
2023-01-30 22:44:53 +0100

    added mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { NotesModule } from './notes/notes.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [AuthModule, UsersModule, NotesModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    NotesModule,
+    MongooseModule.forRoot('mongodb://localhost/priv-notes'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
