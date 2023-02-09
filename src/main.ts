b7de13324dac84eb227735e1d641fe034e5189e7
chemokita13 <chemokita13@gmail.com>
2023-02-09 20:01:14 +0100

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
