b5808425b161e042ea36a4fa34063e4bc6959824
chemokita13 <chemokita13@gmail.com>
2023-03-21 22:28:05 +0100

    env vars better written

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { NotesModule } from './notes/notes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entity/user.entity';
+import { ConfigModule } from '@nestjs/config'
+
 @Module({
   imports: [
     AuthModule,
@@ -16,15 +18,16 @@ import { User } from './users/entity/user.entity';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
-        host: process.env.BDHOST || 'localhost',
+        host: process.env.DBHOST || 'localhost',
         port: 5432,
         username: 'postgres',
-        password: process.env.BDPASS || 'root',
+        password: process.env.DBPASS || 'root',
         database: 'users',
         entities: [User],
         synchronize: true,
       }),
     }),
+    ConfigModule.forRoot({})
   ],
   controllers: [AppController],
   providers: [AppService],
