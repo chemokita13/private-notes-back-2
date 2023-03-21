7436ecfe4f095184cb4ebf2b0a47de587d760dae
chemokita13 <chemokita13@gmail.com>
2023-03-21 20:51:13 +0100

    error solve

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Private notes app')
-    .setDescription('An resAPI with a login, bearer auth and private notes')
+    .setDescription('A resAPI with a login, bearer auth and private notes')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
