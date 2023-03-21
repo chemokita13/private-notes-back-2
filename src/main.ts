47b8cbfca7e6a454426825269a483ac513a8c647
chemokita13 <chemokita13@gmail.com>
2023-03-21 22:28:20 +0100

    origin in an env var

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({ origin: ['http://localhost:3000', process.env.FRONTURI], credentials: true });
   await app.listen(4000);
 }
 bootstrap();
