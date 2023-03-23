be922964425c3f59c0d474656888a87c2bb070ef
chemokita13 <chemokita13@gmail.com>
2023-03-23 20:55:22 +0100

    callback started server added

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors({ origin: ['http://localhost:3000', process.env.FRONTURI], credentials: true });
-  await app.listen(8080);
+  await app.listen(8080, ()=>{console.log(`Server is running on port 8080`)});
 }
 bootstrap();
